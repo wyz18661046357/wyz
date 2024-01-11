c13ec1b7b9c4637e9465fc8a4359da39e4c7a1dd
wyz18661046357 <3536582637@qq.com>
2024-01-12 04:06:38 +0800

    各种代码

---
+++ java/练习/练习4/src/wyz/LogIn.java
@@ -0,0 +1,12 @@
+package wyz;
+
+public class LogIn {
+       public static void main(String[] args){
+    	   System.out.println("\n\t\t欢迎使用我行我素购物管理系统\n");
+    	   System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * * \n");
+    	   System.out.println("\t\t\t1.登录系统\n");
+    	   System.out.println("\t\t\t2.退出\n");
+    	   System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * * \n");
+    	   System.out.println("请选择，输入数字：");
+       }
+}
