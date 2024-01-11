671de066ab3d1442af9d4a968aa404adbf9ec121
wyz18661046357 <3536582637@qq.com>
2024-01-11 11:16:40 +0800

    各种代码

---
+++ java/练习/练习1/src/wyz/HelloWorld.java
@@ -0,0 +1,7 @@
+package wyz;
+
+public class HelloWorld {
+       public static void main(String[] args){
+    	   System.out.println("我的第一个MyEclipse小程序！");
+       }
+}
---
+++ java/练习/练习2/src/wyz/Intraduce.java
@@ -0,0 +1,10 @@
+package wyz;
+
+public class Intraduce {
+       public static void main(String[] args){
+    	   System.out.print("你好！\n");
+    	   System.out.print("姓名：温芸仲\n");
+    	   System.out.print("年龄：21岁\n");
+    	   System.out.print("爱好：读书\n");
+       }
+}
