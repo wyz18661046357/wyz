ba4368233f1a6a8412e5106b268e1e78cd9c8120
wyz18661046357 <3536582637@qq.com>
2024-01-13 18:29:23 +0800

    各种代码

---
+++ java/练习/练习5/src/wyz/Pay.java
@@ -0,0 +1,15 @@
+package wyz;
+
+public class Pay {
+       public static void main(String[] args){
+    	   int shirtPrice=245;
+    	   int shoePrice=570;
+    	   int padPrice=320;
+    	   int shirtNo=2;
+    	   int shoeNo=1;
+    	   int padNo=1;
+    	   double discount=0.8;
+    	   double finalPay=(shirtPrice*shirtNo+shoePrice*shoeNo+padPrice*padNo)*discount;
+    	   System.out.println("消费总金额："+finalPay);
+       }
+}
